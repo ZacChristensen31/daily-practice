297170abd1106352ce89334bfb536b55ee32587e
ZacChristensen31 <zac.d.christensen@gmail.com>
2022-05-19 22:26:39 -0500

    Day 10: CodeWars Solution - Faster Setup

--- CodeWars/010/solve.py
+++ CodeWars/010/solve.py
@@ -7,15 +7,21 @@ from typing import List
     Given an array, return the array with all zeros moved to the end, retaining the order of the other elements.
 """
 
+def fast_solve(arr: List[int]) -> List[int]:
+    stripped = [x for x in arr if x != 0]
+    return stripped + [0] * (len(arr) - len(stripped))
+
 
 def solve(array: List[int]) -> List[int]:
     return sorted(array, key=lambda x: x == 0)
 
 
 if __name__ == '__main__':
-    assert solve([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]) == [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
-    assert solve([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]) == [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0,
+    target_solve = fast_solve
+
+    assert target_solve([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]) == [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
+    assert target_solve([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]) == [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0,
                                                                                    0, 0, 0, 0, 0, 0, 0, 0]
-    assert solve([0, 0]) == [0, 0]
-    assert solve([0, 1, 0]) == [1, 0, 0]
-    assert solve([]) == []
+    assert target_solve([0, 0]) == [0, 0]
+    assert target_solve([0, 1, 0]) == [1, 0, 0]
+    assert target_solve([]) == []
