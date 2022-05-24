90904b3053be2932eae13a40b6b1cd881bac892e
ZacChristensen31 <zac.d.christensen@gmail.com>
2022-05-23 22:07:23 -0500

    Day 13-14: Setup

---
+++ CodeWars/014/solve.py
@@ -0,0 +1,8 @@
+# Prpblem comes from CodeWars
+
+def solve():
+    pass
+
+
+if __name__ == '__main__':
+    solve()
---
+++ LeetCode/013/solve.py
@@ -0,0 +1,17 @@
+# Problem comes from LeetCode https://leetcode.com/problems/ones-and-zeroes/
+# Solved on Sunday May 22nd, 2022
+
+from typing import List
+
+"""
+    Given a list of binary strings, a max number of zeros m, and a max number of ones n, return the max number of strings
+    you can select while staying under m and n.
+"""
+
+
+def solve(nums: List[str], m: int, n: int) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    pass
