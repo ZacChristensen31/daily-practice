cdfd41101461161d96898d18d4949e7bd83354b6
ZacChristensen31 <zac.d.christensen@gmail.com>
2022-05-24 19:58:26 -0500

    Day 14: CodeWars Solution

--- CodeWars/014/solve.py
+++ CodeWars/014/solve.py
@@ -1,8 +1,18 @@
-# Prpblem comes from CodeWars
+# Problem comes from CodeWars https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/python
+# Solved on Monday May 23rd, 2022
 
-def solve():
-    pass
+from math import sqrt, floor
+
+"""
+    Given a number n, return -1 if the number is not a square. If it is a perfect square, return the next perfect square.
+"""
+
+def solve(n: int) -> int:
+    return -1 if sqrt(n) != floor(sqrt(n)) else (sqrt(n) + 1) ** 2
+
 
 
 if __name__ == '__main__':
-    solve()
+    assert solve(1) == 4
+    assert solve(121) == 144
+    assert solve(2) == -1
