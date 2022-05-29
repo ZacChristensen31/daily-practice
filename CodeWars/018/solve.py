f63dbdb985cf63e5805cf46e9df78bdb552e5ef4
ZacChristensen31 <zac.d.christensen@gmail.com>
2022-05-28 23:42:06 -0500

    Day 18: LeetCode and CodeWars Setup

---
+++ CodeWars/018/solve.py
@@ -0,0 +1,7 @@
+# Problem comes from CodeWars
+
+def solve():
+    pass
+
+if __name__ == '__main__':
+    solve()
---
+++ LeetCode/018/solve.py
@@ -0,0 +1,7 @@
+# Problem comes from LeetCode
+
+def solve():
+    pass
+
+if __name__ == '__main__':
+    solve()
